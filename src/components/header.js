75f75afbe28ff532db1a6ba8c5f8693158968b83
Chris DeMars <cdemars86@gmail.com>
2020-04-06 18:25:21 -0600

    some more stuff

--- src/components/header.js
+++ src/components/header.js
@@ -4,40 +4,36 @@ import { Link } from "gatsby"
 import "../global/styles.scss"
 import HeaderStyles from "../styles/header.module.scss"
 
-// import Headshot from "../images/headshot.png"
-
 const Header = () => {
   return (
-    <>
-      <header className={HeaderStyles.header}>
-        <h1>ChrisDeMars</h1>
-        <nav>
-          <ul className={HeaderStyles.navItems}>
-            <li className={HeaderStyles.navItem}>
-              <Link to="/">Home</Link>
-            </li>
-            <li className={HeaderStyles.navItem}>
-              <Link to="/about">About</Link>
-            </li>
-            <li className={HeaderStyles.navItem}>
-              <Link to="/blog">Blog</Link>
-            </li>
-            <li className={HeaderStyles.navItem}>
-              <Link to="/blog">Portfolio</Link>
-            </li>
-            <li className={HeaderStyles.navItem}>
-              <Link to="/blog">Events</Link>
-            </li>
-            <li className={HeaderStyles.navItem}>
-              <Link to="/blog">Resume</Link>
-            </li>
-            <li className={HeaderStyles.navItem}>
-              <Link to="/blog">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </>
+    <header className={HeaderStyles.header}>
+      <h1>ChrisDeMars</h1>
+      <nav>
+        <ul className={HeaderStyles.navItems}>
+          <li className={HeaderStyles.navItem}>
+            <Link to="/">Home</Link>
+          </li>
+          <li className={HeaderStyles.navItem}>
+            <Link to="/about">About</Link>
+          </li>
+          <li className={HeaderStyles.navItem}>
+            <Link to="/blog">Blog</Link>
+          </li>
+          <li className={HeaderStyles.navItem}>
+            <Link to="/blog">Portfolio</Link>
+          </li>
+          <li className={HeaderStyles.navItem}>
+            <Link to="/blog">Events</Link>
+          </li>
+          <li className={HeaderStyles.navItem}>
+            <Link to="/blog">Resume</Link>
+          </li>
+          <li className={HeaderStyles.navItem}>
+            <Link to="/blog">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
--- src/components/hero.js
+++ src/components/hero.js
@@ -7,20 +7,20 @@ import Headshot from "../images/headshot.png"
 
 const Hero = () => {
   return (
-    <>
-      <section className={HeroStyles.heroSection}>
-        <div className={HeroStyles.headshotSection}>
-          <img src={Headshot} alt="" />
-        </div>
-        <div className={HeroStyles.miniBio}>
-          <h1>Hey there</h1>
-          <h2>What is good?</h2>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab, earum!
-          </p>
-        </div>
-      </section>
-    </>
+    <section className={HeroStyles.heroSection}>
+      <div className={HeroStyles.headshotSection}>
+        <img src={Headshot} alt="" />
+      </div>
+      <div className={HeroStyles.miniBio}>
+        <h1>Hey there, I'm Chris!</h1>
+        <p>
+          Some would say I am a front-end developer. Possibly a CSS developer.
+          Some would say web accessibility is my thing. I just like to say I am
+          a builder of the web and that is what I love to do. I also love to
+          speak, write, and teach about the web.
+        </p>
+      </div>
+    </section>
   )
 }
 
